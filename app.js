ebfbb8cc410beafa93ee8c6201c98a597cd4ec1c
Zhiran Sun <zhirannature@gmail.com>
2018-12-03 13:38:54 +0000

    Find some ESLint Error but not change them all

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-const books = require('./routes/books');
+var books = require('./routes/books');
 var app = express();
 
 // view engine setup
@@ -40,7 +40,7 @@ app.use(function(req, res, next) {
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function(err, req, res) {
     // set locals, only providing error in development
     res.locals.message = err.message;
     res.locals.error = req.app.get('env') === 'development' ? err : {};
